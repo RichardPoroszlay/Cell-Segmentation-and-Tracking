04fa7ca6079b57b951464cbbbac2ad0734587eb1
Richard Poroszlay <richardporoszlay@gmail.com>
2024-09-09 17:57:12 +0200

    Use two imgsz value types for training

--- src/train_locally.py
+++ src/train_locally.py
@@ -8,4 +8,5 @@ ROOT_DIR = "C:/Users/Richard/Desktop/Cell-Segmentation-and-Tracking/src/"
 model = YOLO("yolov8l.pt")  # load pre trained model
 
 # Use the model
-results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), epochs=50)  # train the model
+results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=1030 ,epochs=50)  # train the model
+# results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=(1300, 1030) ,epochs=50)  # train the model
