91165fa5937683a677db040a996bf3c11d7dad53
Richard Poroszlay <richardporoszlay@gmail.com>
2024-09-09 18:27:11 +0200

    Change the values in the tuple

--- src/train_locally.py
+++ src/train_locally.py
@@ -9,4 +9,4 @@ model = YOLO("yolov8l.pt")  # load pre trained model
 
 # Use the model
 results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=1030 ,epochs=50)  # train the model
-# results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=(1300, 1030) ,epochs=50)  # train the model
+# results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=(1030, 1300) ,epochs=50)  # train the model
