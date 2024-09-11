5f02cb8ab13b38f9e0f6bd941448779f7f54430e
Richard Poroszlay <richardporoszlay@gmail.com>
2024-09-11 16:21:22 +0200

    Change some parameter values

--- src/train_locally.py
+++ src/train_locally.py
@@ -5,8 +5,8 @@ from ultralytics import YOLO
 ROOT_DIR = "C:/Users/Richard/Desktop/Cell-Segmentation-and-Tracking/src/"
 
 # Load a model
-model = YOLO("yolov8l.pt")  # load pre trained model
+model = YOLO("yolov8x.pt")  # load pre trained model
 
 # Use the model
-results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=1030 ,epochs=50)  # train the model
-# results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=(1030, 1300) ,epochs=50)  # train the model
+results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=1030, epochs=100)  # train the model
+# results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=(1030, 1300), epochs=100)  # train the model
