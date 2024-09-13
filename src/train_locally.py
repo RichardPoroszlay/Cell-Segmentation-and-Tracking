76b60fb2305683fec9d0f3af35803a4b5261fed5
Richard Poroszlay <richardporoszlay@gmail.com>
2024-09-13 12:12:30 +0200

    Use the P2 architecture

--- src/train_locally.py
+++ src/train_locally.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 ROOT_DIR = "C:/Users/Richard/Desktop/Cell-Segmentation-and-Tracking/src/"
 
 # Load a model
-model = YOLO("yolov8x.pt")  # load pre trained model
+model = YOLO("yolov8x-p2.yaml").load("yolov8x.pt")  # load pre trained model
 
 # Use the model
 results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=1030, epochs=100)  # train the model
