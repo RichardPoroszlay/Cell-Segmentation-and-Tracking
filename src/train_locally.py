4d59381b8c3489fab036c4c78ad24adf92de3046
Richard Poroszlay <richardporoszlay@gmail.com>
2024-09-13 12:18:23 +0200

    Change the value of the epoch argument

--- src/train_locally.py
+++ src/train_locally.py
@@ -8,5 +8,4 @@ ROOT_DIR = "C:/Users/Richard/Desktop/Cell-Segmentation-and-Tracking/src/"
 model = YOLO("yolov8x-p2.yaml").load("yolov8x.pt")  # load pre trained model
 
 # Use the model
-results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=1030, epochs=100)  # train the model
-# results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=(1030, 1300), epochs=100)  # train the model
+results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=(1030, 1300), epochs=200)  # train the model
