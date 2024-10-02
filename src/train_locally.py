3c07a3890be2d79171a2cbb387896e42697e812f
Richard Poroszlay <richardporoszlay@gmail.com>
2024-10-02 17:16:23 +0200

    Adjust some settings

--- src/train_locally.py
+++ src/train_locally.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 ROOT_DIR = "C:/Users/Richard/Desktop/Cell-Segmentation-and-Tracking/src/"
 
 # Load a model
-model = YOLO("yolov8x-p2.yaml").load("yolov8x.pt")  # load pre trained model
+model = YOLO("yolov8x.pt")  # load pre trained model
 
 # Use the model
-results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), imgsz=(1030, 1300), epochs=200)  # train the model
+results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), epochs=50)  # train the model
