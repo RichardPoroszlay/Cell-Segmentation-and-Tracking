251e97082bd821499828aac914284425ba1e202e
Richard Poroszlay <richardporoszlay@gmail.com>
2024-11-13 13:45:03 +0100

    Add the 'mixed' arguments to model.train

--- src/train_locally.py
+++ src/train_locally.py
@@ -8,4 +8,4 @@ ROOT_DIR = "root-dir"
 model = YOLO("yolov8x.pt")  # load pre trained model
 
 # Use the model
-results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), epochs=100)  # train the model
+results = model.train(data=os.path.join(ROOT_DIR, "config.yaml"), cos_lr=True, patience=5, verbose=True, epochs=100)  # train the model
